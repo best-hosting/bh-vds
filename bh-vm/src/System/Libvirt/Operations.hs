0b5dbb3acea017046864b42ea89a918ea3cfe2e0
sgf <sgf.dma@gmail.com>
2017-09-07 11:06:58 +0300

    chg: libvirt. Define wrapper functions for calling `virsh`.
    This allows to gather all `#ifndef TEST` into one place.

--- bh-vm/src/System/Libvirt/Operations.hs
+++ bh-vm/src/System/Libvirt/Operations.hs
@@ -11,100 +11,102 @@
 module System.Libvirt.Operations
   where
 
+import           Data.Text (pack, dropWhileEnd)
 import qualified Filesystem.Path.CurrentOS  as F
 import           TextShow (showt)
 import           Control.Monad.Except
 import           Turtle
 import           Control.Foldl (list)
 import           Control.Monad.Managed
-#ifndef TEST
-import           Data.Text (pack)
 import           System.Directory
-#endif
 
 import           System.Libvirt.Types
 
 import           Internal.Common
 
 
+-- $virsh
+
+-- | Call to @virsh@ do not expecting any output.
+virsh :: MonadIO m => [Text] -> Shell Line -> m ()
+#ifndef TEST
+virsh argv inp      = procs "virsh" argv inp
+#else
+virsh ("vol-create" : _) _  = return ()
+virsh ("define"     : _) _  = return ()
+virsh argv               _  = error $
+    "virsh: Unsupported virsh options for test mode: " ++ show argv
+#endif
+
+-- | Call to @virsh@ expecting some output.
+invirsh :: [Text] -> Shell Line -> Shell Line
+#ifndef TEST
+invirsh argv inp    = inproc "virsh" argv inp
+#else
+invirsh ("list" : _) _
+                    = select . textToLines $ "test4\ntest5"
+invirsh ["dumpxml", n] _
+                    = input $ "./test/configs" </> fromText n <.> "xml"
+invirsh ["vol-dumpxml", p] _
+                    = let n = F.encodeString (F.basename (fromText p))
+                      in  input $ "./test/configs"
+                                </> F.decodeString (n <> "-vol.xml")
+invirsh ["vol-path", "--pool", p, n] _
+                    = select . textToLines . pack . F.encodeString $
+                        "/dev"  </> fromText p </> fromText n
+invirsh argv _      = error $
+    "invirsh: Unsupported virsh options for test mode: " ++ show argv
+#endif
+
 -- | @virsh list --all@
 virshListAll :: MonadIO m => m [Name]
-virshListAll        =
-#ifndef TEST
-    flip fold list $ do
-        ns <- grep chars1 $ inproc "virsh" ["list", "--all", "--name"] empty
-        either (const empty) return . parseName $ lineToText ns
-#else
-    return ["test4", "test5"]
-#endif
+virshListAll        = flip fold list $ do
+    ns <- grep chars1 $ invirsh ["list", "--all", "--name"] empty
+    either (const empty) return . parseName $ lineToText ns
 
 -- | @virsh dumpxml@
 virshDumpXml :: MonadIO m => Name -> m Text
-virshDumpXml n      =
-#ifndef TEST
-    strict $ inproc "virsh" ["dumpxml", showt n] empty
-#else
-    liftIO . readTextFile $
-        "./test/configs" </> fromText (showt n) <.> "xml"
-#endif
+virshDumpXml n      = strict $ invirsh  ["dumpxml", showt n] empty
 
 -- | @virsh vol-dumpxml@
 -- Note, that @vol-dumpxml@ may work on /full path/ to volume /without/ pool
 -- name specified.
 virshVolDumpXml :: MonadIO m => F.FilePath -> m Text
-virshVolDumpXml p   =
-#ifndef TEST
-    strict $ inproc "virsh" ["vol-dumpxml", pack (F.encodeString p)] empty
-#else
-    liftIO . readTextFile $ "./test/configs" </>
-        F.decodeString (F.encodeString (F.basename p) <> "-vol.xml")
-#endif
+virshVolDumpXml p   = strict $ invirsh  [ "vol-dumpxml"
+                                        , pack (F.encodeString p)
+                                        ]
+                                        empty
 
 -- | @virsh vol-create@
 virshVolCreate :: MonadManaged m =>
                     Volume -> Text -> m ()
 virshVolCreate Volume{..} t = do
-#ifndef TEST
     tmp <- liftIO getTemporaryDirectory
     tf  <- mktempfile (F.decodeString tmp) "virshVolCreate"
     liftIO $ writeTextFile tf t
-    sh $ procs "virsh"
+    sh $ virsh
             [ "vol-create"
             , "--pool", showt (fromLast volPool)
             , pack (F.encodeString tf)
             ]
             empty
-#else
-    return ()
-#endif
 
--- | @virsh vol-path@
+-- | @virsh vol-path@ . Note, newline in filenames /not/ supported.
 virshVolPath :: MonadIO m => Volume -> m F.FilePath
-virshVolPath Volume{..} =
-#ifndef TEST
-    fmap fromText . strict . grep chars1 $
-        inproc "virsh"
-            [ "vol-path"
-            , "--pool", showt (fromLast volPool), showt volName
-            ]
-            empty
-#else
-    return $
-        "/dev" </> fromText (showt (fromLast volPool))
-               </> fromText (showt volName)
-#endif
+virshVolPath Volume{..} = fmap (fromText . dropWhileEnd (== '\n')) . strict $
+    invirsh
+        [ "vol-path"
+        , "--pool", showt (fromLast volPool), showt volName
+        ]
+        empty
 
 -- | @virsh define@
 virshDefine :: MonadManaged m => Domain -> Text -> m ()
 virshDefine _ t     = do
-#ifndef TEST
     -- FIXME: Wrapper around `getTemporaryDirectory` and `mktempfile` and,
     -- probably, `writeTextFile` ?
     tmp <- liftIO getTemporaryDirectory
     tf  <- mktempfile (F.decodeString tmp) "virshVolCreate"
     liftIO $ writeTextFile tf t
-    sh $ procs "virsh" ["define", pack (F.encodeString tf)] empty
-#else
-    return ()
-#endif
+    sh $ virsh ["define", pack (F.encodeString tf)] empty
 
